353db2e4b6a851f58673d60c0fe038608ac2378c
T-rav <tmfrisinger@gmail.com>
2018-05-30 09:31:49 +0200

    Moved Dapper queries into DataService

---
+++ Dapper.Demo.Tests/CustomerDataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper.Demo.Tests.Models.Customer;
+
+namespace Dapper.Demo.Tests
+{
+    public class CustomerDataService
+    {
+        private string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Initial Catalog=DapperDemo";
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT Id,Name,Email,Login,TimeZoneId,ExternalSystemId FROM dbo.Customers";
+                var customers = connection.Query<Customer>(sql);
+                return customers;
+            }
+        }
+    }
+}
